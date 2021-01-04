76c704a9a9bbb190bfdbd330c0f0c421433186fc
Your Name <you@example.com>
2021-01-04 21:37:32 +0800

    BaseMapper

--- src/main/java/com/qy25/sm/mapper/SupperMapper.java
+++ src/main/java/com/qy25/sm/mapper/SupperMapper.java
@@ -2,30 +2,11 @@ package com.qy25.sm.mapper;
 
 import com.qy25.sm.entity.Supper;
 import com.qy25.sm.entity.SupperExample;
+import com.qy25.sm.mapper.base.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface SupperMapper {
-    long countByExample(SupperExample example);
+public interface SupperMapper extends BaseMapper<Supper,Long> {
 
-    int deleteByExample(SupperExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(Supper record);
-
-    int insertSelective(Supper record);
-
-    List<Supper> selectByExample(SupperExample example);
-
-    Supper selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") Supper record, @Param("example") SupperExample example);
-
-    int updateByExample(@Param("record") Supper record, @Param("example") SupperExample example);
-
-    int updateByPrimaryKeySelective(Supper record);
-
-    int updateByPrimaryKey(Supper record);
 }
